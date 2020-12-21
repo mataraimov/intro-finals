6b7126f9c34b5e8088a34b5373b42d1a6ebe63ca
Sakubek1337 <73515656+Sakubek1337@users.noreply.github.com>
2020-12-22 05:16:39 +0600

    Update arcanoid.py

--- arcanoid.py
+++ arcanoid.py
@@ -3,6 +3,30 @@ import sys
 from random import randrange as rnd
 from random import choice as rch
 
+
+def score_dis(scoree, highscoree):
+    if is_going:
+        scoresur = game_font.render(f'Score: {scoree}', True, (255, 255, 255))
+        score_rect = scoresur.get_rect(center=(800, 400))
+        screen.blit(scoresur, score_rect)
+    elif not is_going and lost:
+        scoresur = game_font2.render(f'Score: {scoree}', True, (255, 255, 255))
+        score_rect = scoresur.get_rect(center=(450, 500))
+        screen.blit(scoresur, score_rect)
+
+        highscoresur = game_font2.render(f'High score: {highscoree}', True, (255, 255, 255))
+        highscore_rect = highscoresur.get_rect(center=(450, 75))
+        screen.blit(highscoresur, highscore_rect)
+
+        highscoresurs = game_font2.render(f'PRESS SPACE TO PLAY AGAIN!', True, (255, 255, 255))
+        highscore_rects = highscoresurs.get_rect(center=(450, 550))
+        screen.blit(highscoresurs, highscore_rects)
+    elif not is_going and not lost:
+        highscoresurse = game_font2.render(f'PRESS SPACE TO GO ON NEXT LEVEL!', True, (255, 255, 255))
+        highscore_rectse = highscoresurse.get_rect(center=(450, 550))
+        screen.blit(highscoresurse, highscore_rectse)
+
+
 width, height = 900, 600
 framerate = 60
 pygame.init()
@@ -33,21 +57,60 @@ paddle = padd.get_rect(center=(450, 575))
 #
 bg = pygame.image.load('cosmos.jpg')
 ball_rad = 15
-ball_speed = 6
+ball_speed = 5
 ball_rect = int(ball_rad * 2 ** 0.5)
 ball = pygame.Rect(rnd(ball_rect, width - ball_rect), height // 2, ball_rect, ball_rect)
 dx = 1
 dy = -1
 
 blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11) for j in range(4)]
-res_blocks = blocks
+res_blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11) for j in range(4)]
 
+game_font = pygame.font.Font('fb.TTF', 18)
+game_font2 = pygame.font.Font('fb.TTF', 32)
+score = 0
+highscore = 0
+points = 1
 is_going = True
+lost = False
+touched = False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not is_going and lost:
+                is_going = True
+                blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11) for j in range(4)]
+                colorz = []
+                for k in range(6):
+                    for i in range(7):
+                        colorz.extend((blue, light_blue, red, green, pink, yellow))
+                colorz.extend((blue, light_blue))
 
+                ball.center = (rnd(ball_rect, width - ball_rect), height // 2)
+                dx = 1
+                dy = -1
+                score = 0
+                speed = 15
+                ball_speed = 5
+                lost = False
+            elif event.key == pygame.K_SPACE and not is_going and not lost:
+                is_going = True
+                blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11) for j in range(4)]
+                colorz = []
+                for k in range(6):
+                    for i in range(7):
+                        colorz.extend((blue, light_blue, red, green, pink, yellow))
+                colorz.extend((blue, light_blue))
+
+                ball.center = (rnd(ball_rect, width - ball_rect), height // 2)
+                dx = 1
+                dy = -1
+                points *= 2
+                speed += 2
+                ball_speed += 2
     screen.blit(bg, (0, 0))
     #
     if is_going:
@@ -63,14 +126,16 @@ while True:
         key = pygame.key.get_pressed()
         if ball.centery < 15:
             dy = -dy
+            touched = False
         if ball.centerx > 885 or ball.centerx < 15:
             dx = -dx
+            touched = False
         if key[pygame.K_LEFT] and paddle.left > 0:
             paddle.left -= speed
         if key[pygame.K_RIGHT] and paddle.right < 900:
             paddle.right += speed
 
-        if ball.colliderect(paddle):
+        if ball.colliderect(paddle) and not touched:
             if dx > 0:
                 deltax = ball.right - paddle.left
             else:
@@ -79,11 +144,14 @@ while True:
                 deltay = ball.bottom - paddle.top
             else:
                 deltay = paddle.bottom - ball.top
-            if deltax > deltay:
+            if abs(deltax - deltay) < 10:
+                dx, dy = -dx, -dy
+            elif deltax > deltay:
                 dy = -dy
             elif deltay > deltax:
                 dx = -dx
                 dy = -dy
+            touched = True
         for block in blocks:
             if ball.colliderect(block):
                 if dx > 0:
@@ -101,12 +169,35 @@ while True:
                 s = blocks.index(block)
                 colorz.pop(s)
                 blocks.pop(s)
+                score += points
+                touched = False
+
         #
-        if ball.centery > 600:
+        if ball.bottom >= 600:
             is_going = False
-            colorz = res_cols
-            blocks = res_blocks
+            touched = False
+            lost = True
+
+        if len(blocks) == 0:
+            is_going = False
+            lost = False
+
+        if highscore < score:
+            highscore = score
+        score_dis(score, highscore)
+
+    if not is_going and lost:
+        gameover = pygame.image.load('gameover.png').convert_alpha()
+        gameover_rect = gameover.get_rect(center=(450, 300))
+        screen.blit(gameover, gameover_rect)
+        score_dis(score, highscore)
+    elif not is_going:
+        gratz = pygame.image.load('gratz.png').convert_alpha()
+        gratz_rect = gratz.get_rect(center=(450, 300))
+        screen.blit(gratz, gratz_rect)
+        score_dis(score, highscore)
     #
+
     pygame.display.flip()
 
     clock.tick(framerate)
