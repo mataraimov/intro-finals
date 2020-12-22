a3aa8127b6c0cdaa882049c0f595c644a901f7ee
Sakubek1337 <73515656+Sakubek1337@users.noreply.github.com>
2020-12-22 06:42:06 +0600

    PEP8 adjustments

--- arcanoid_game.py
+++ arcanoid_game.py
@@ -14,15 +14,18 @@ def score_dis(scoree, highscoree):
         score_rect = scoresur.get_rect(center=(450, 500))
         screen.blit(scoresur, score_rect)
 
-        highscoresur = game_font2.render(f'High score: {highscoree}', True, (255, 255, 255))
+        highscoresur = game_font2.render(f'High score: {highscoree}', True,
+                                         (255, 255, 255))
         highscore_rect = highscoresur.get_rect(center=(450, 75))
         screen.blit(highscoresur, highscore_rect)
 
-        highscoresurs = game_font2.render(f'PRESS SPACE TO PLAY AGAIN!', True, (255, 255, 255))
+        highscoresurs = game_font2.render(f'PRESS SPACE TO PLAY AGAIN!', True,
+                                          (255, 255, 255))
         highscore_rects = highscoresurs.get_rect(center=(450, 550))
         screen.blit(highscoresurs, highscore_rects)
     elif not is_going and not lost:
-        highscoresurse = game_font2.render(f'PRESS SPACE TO GO ON NEXT LEVEL!', True, (255, 255, 255))
+        highscoresurse = game_font2.render(f'PRESS SPACE TO GO ON NEXT LEVEL!',
+                                           True, (255, 255, 255))
         highscore_rectse = highscoresurse.get_rect(center=(450, 550))
         screen.blit(highscoresurse, highscore_rectse)
 
@@ -35,12 +38,18 @@ pygame.display.set_caption("Arcanoid by MS & MN")
 clock = pygame.time.Clock()
 
 #  colors
-blue = pygame.transform.scale(pygame.image.load('blue_block.png'), (80, 40)).convert_alpha()
-light_blue = pygame.transform.scale(pygame.image.load('light_blue_block.png'), (80, 40)).convert_alpha()
-green = pygame.transform.scale(pygame.image.load('green_block.png'), (80, 40)).convert_alpha()
-pink = pygame.transform.scale(pygame.image.load('pink_block.png'), (80, 40)).convert_alpha()
-red = pygame.transform.scale(pygame.image.load('red_block.png'), (80, 40)).convert_alpha()
-yellow = pygame.transform.scale(pygame.image.load('yellow_block.png'), (80, 40)).convert_alpha()
+blue = pygame.transform.scale(pygame.image.load('blue_block.png'),
+                              (80, 40)).convert_alpha()
+light_blue = pygame.transform.scale(pygame.image.load('light_blue_block.png'),
+                                    (80, 40)).convert_alpha()
+green = pygame.transform.scale(pygame.image.load('green_block.png'),
+                               (80, 40)).convert_alpha()
+pink = pygame.transform.scale(pygame.image.load('pink_block.png'),
+                              (80, 40)).convert_alpha()
+red = pygame.transform.scale(pygame.image.load('red_block.png'),
+                             (80, 40)).convert_alpha()
+yellow = pygame.transform.scale(pygame.image.load('yellow_block.png'),
+                                (80, 40)).convert_alpha()
 #
 colorz = []
 for k in range(6):
@@ -52,19 +61,23 @@ res_cols = colorz
 padw = 106
 padh = 28
 speed = 15
-padd = pygame.transform.scale(pygame.image.load('paddle_cr.png'), (padw, padh)).convert_alpha()
+padd = pygame.transform.scale(pygame.image.load('paddle_cr.png'),
+                              (padw, padh)).convert_alpha()
 paddle = padd.get_rect(center=(450, 575))
 #
 bg = pygame.image.load('cosmos.jpg')
 ball_rad = 15
 ball_speed = 5
 ball_rect = int(ball_rad * 2 ** 0.5)
-ball = pygame.Rect(rnd(ball_rect, width - ball_rect), height // 2, ball_rect, ball_rect)
+ball = pygame.Rect(rnd(ball_rect, width - ball_rect), height // 2, ball_rect,
+                   ball_rect)
 dx = 1
 dy = -1
 
-blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11) for j in range(4)]
-res_blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11) for j in range(4)]
+blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11)
+          for j in range(4)]
+res_blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11)
+              for j in range(4)]
 
 game_font = pygame.font.Font('fb.TTF', 18)
 game_font2 = pygame.font.Font('fb.TTF', 32)
@@ -82,11 +95,13 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and not is_going and lost:
                 is_going = True
-                blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11) for j in range(4)]
+                blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40)
+                          for i in range(11) for j in range(4)]
                 colorz = []
                 for k in range(6):
                     for i in range(7):
-                        colorz.extend((blue, light_blue, red, green, pink, yellow))
+                        colorz.extend((blue, light_blue, red, green,
+                                       pink, yellow))
                 colorz.extend((blue, light_blue))
 
                 ball.center = (rnd(ball_rect, width - ball_rect), height // 2)
@@ -98,11 +113,13 @@ while True:
                 lost = False
             elif event.key == pygame.K_SPACE and not is_going and not lost:
                 is_going = True
-                blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40) for i in range(11) for j in range(4)]
+                blocks = [pygame.Rect(10 + 80*i, 10 + 40*j, 80, 40)
+                          for i in range(11) for j in range(4)]
                 colorz = []
                 for k in range(6):
                     for i in range(7):
-                        colorz.extend((blue, light_blue, red, green, pink, yellow))
+                        colorz.extend((blue, light_blue, red, green, pink,
+                                       yellow))
                 colorz.extend((blue, light_blue))
 
                 ball.center = (rnd(ball_rect, width - ball_rect), height // 2)
@@ -120,7 +137,8 @@ while True:
             screen.blit(colorz[i], (h.x, h.y))
             i += 1
 
-        pygame.draw.circle(screen, pygame.Color('white'), ball.center, ball_rad)
+        pygame.draw.circle(screen, pygame.Color('white'), ball.center,
+                           ball_rad)
         ball.x += ball_speed * dx
         ball.y += ball_speed * dy
         key = pygame.key.get_pressed()
